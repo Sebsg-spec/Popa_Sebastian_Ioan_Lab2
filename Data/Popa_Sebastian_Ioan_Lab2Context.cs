88b6e7b4bfacc6b61e6fd523dce25dfb83395729
sebsg-spec <popasebi122@gmail.com>
2023-10-23 18:56:43 +0300

    Update lab 3

--- Data/Popa_Sebastian_Ioan_Lab2Context.cs
+++ Data/Popa_Sebastian_Ioan_Lab2Context.cs
@@ -19,5 +19,7 @@ namespace Popa_Sebastian_Ioan_Lab2.Data
         public DbSet<Popa_Sebastian_Ioan_Lab2.Models.Publisher>? Publisher { get; set; }
 
         public DbSet<Popa_Sebastian_Ioan_Lab2.Models.Author>? Author { get; set; }
+
+        public DbSet<Popa_Sebastian_Ioan_Lab2.Models.Category>? Category { get; set; }
     }
 }
--- Migrations/20231023155518_BookMigration.Designer.cs
+++ Migrations/20231023155518_BookMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Popa_Sebastian_Ioan_Lab2.Data;
 
@@ -11,9 +12,10 @@ using Popa_Sebastian_Ioan_Lab2.Data;
 namespace Popa_Sebastian_Ioan_Lab2.Migrations
 {
     [DbContext(typeof(Popa_Sebastian_Ioan_Lab2Context))]
-    partial class Popa_Sebastian_Ioan_Lab2ContextModelSnapshot : ModelSnapshot
+    [Migration("20231023155518_BookMigration")]
+    partial class BookMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
