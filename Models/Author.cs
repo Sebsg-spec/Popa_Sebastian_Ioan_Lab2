3443ce9e4cfdf6aa3c6809fd54c7bb4e4f9657ec
sebsg-spec <popasebi122@gmail.com>
2023-10-30 18:28:03 +0200

    Update Lab4

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,11 +1,19 @@
-﻿namespace Popa_Sebastian_Ioan_Lab2.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace Popa_Sebastian_Ioan_Lab2.Models
 {
     public class Author
     {
         public int ID { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
-        
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
         public ICollection<Book>? Books { get; set; }
     }
 }
